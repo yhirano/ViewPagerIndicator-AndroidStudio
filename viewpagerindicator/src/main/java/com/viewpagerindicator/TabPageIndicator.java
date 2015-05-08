841319eb4b72a00de81acecc6edc3289e0b6f8ea
Y.Hirano <chirano@uraroji.com>
2015-05-08 11:55:36 +0900

    Cancel tab text ellipsize.

--- viewpagerindicator/src/main/java/com/viewpagerindicator/TabPageIndicator.java
+++ viewpagerindicator/src/main/java/com/viewpagerindicator/TabPageIndicator.java
@@ -269,11 +269,11 @@ public class TabPageIndicator extends HorizontalScrollView implements PageIndica
         public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-            // Re-measure if we went beyond our maximum size.
-            if (mMaxTabWidth > 0 && getMeasuredWidth() > mMaxTabWidth) {
-                super.onMeasure(MeasureSpec.makeMeasureSpec(mMaxTabWidth, MeasureSpec.EXACTLY),
-                        heightMeasureSpec);
-            }
+//            // Re-measure if we went beyond our maximum size.
+//            if (mMaxTabWidth > 0 && getMeasuredWidth() > mMaxTabWidth) {
+//                super.onMeasure(MeasureSpec.makeMeasureSpec(mMaxTabWidth, MeasureSpec.EXACTLY),
+//                        heightMeasureSpec);
+//            }
         }
 
         public int getIndex() {
